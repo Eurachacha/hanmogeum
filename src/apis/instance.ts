5507bb9c0c0deb8274c58fbe6256d176f807f22b
youngchae <010101chae@gmail.com>
2023-11-28 14:41:14 +0900

    setting(@/apis): api 인스턴스 생성 (#3)
    api 인스턴스를 생성하고 timeout 값과 토큰 저장 키를 상수화했습니다.
    
    Co-authored-by: Yeonju Oh <Oh5Yeonju@users.noreply.github.com>
    Co-authored-by: jungmyungjin <jungmyungjin@users.noreply.github.com>

---
+++ src/apis/instance.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { INSTANCE_TIMEOUT, AUTH_TOKEN_KEY } from "@/constants/api";
+
+const publicInstance = axios.create({
+  baseURL: process.env.API_BASE_URL,
+  timeout: INSTANCE_TIMEOUT,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const privateInstance = axios.create({
+  baseURL: process.env.API_BASE_URL,
+  timeout: INSTANCE_TIMEOUT,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 헤더에 토큰을 전달하는 요청 인터셉터
+privateInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(AUTH_TOKEN_KEY);
+    const newConfig = { ...config };
+    if (token) {
+      newConfig.headers.Authorization = `Bearer ${token}`;
+    }
+    return newConfig;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// 에러를 처리하는 응답 인터셉터
+privateInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// 에러를 처리하는 응답 인터셉터
+publicInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export { publicInstance, privateInstance };
---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const INSTANCE_TIMEOUT = 2000;
+export const AUTH_TOKEN_KEY = "token";
