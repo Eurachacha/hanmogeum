7b1f37e7bc8914312c03089408a47b6b5247b642
jungmyungjin <myung2667@gmail.com>
2023-12-02 23:38:02 +0900

    feat(Login): 로그인 페이지 구현 (#37)
    로그인 페이지의 Container 와 Page 컴포넌트를 구현하였습니다.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,14 @@
+import ContentsTitle from "@/components/ContentsTitle/ContentsTitle";
+import LoginContainer from "@/containers/LoginContainer/LoginContainer";
+
 const LoginPage = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <div>
+      로그인 페이지
+      <ContentsTitle title="로그인" />
+      <LoginContainer />
+    </div>
+  );
 };
 
 export default LoginPage;
