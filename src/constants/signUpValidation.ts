f5a98e60a99adfd4bfa49264b1412abe2efc36ca
jungmyungjin <myung2667@gmail.com>
2023-12-04 17:48:02 +0900

    feat(SignUp): 회원가입 페이지, 컨테이너 구현
    카카오 지도 API 연결을 제외한
    회원가입 페이지, 컨테이너를 구현하였습니다.

---
+++ src/constants/signUpValidation.ts
@@ -0,0 +1,3 @@
+const PASSWORD_MIN_LENGTH = 8;
+
+export default PASSWORD_MIN_LENGTH;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,13 @@
+import ContentsTitle from "@/components/ContentsTitle/ContentsTitle";
+import SignUpContainer from "@/containers/SignUpContainer/SignUpContainer";
+
 const SignUpPage = () => {
-  return <div>회원가입 페이지</div>;
+  return (
+    <div>
+      <ContentsTitle title="회원가입" />
+      <SignUpContainer />
+    </div>
+  );
 };
 
 export default SignUpPage;
