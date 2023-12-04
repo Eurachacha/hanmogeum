6275d62d08d10c903af813248d72d6b40582b527
youngchae <010101chae@gmail.com>
2023-12-04 17:18:14 +0900

    feat(orderCheckout): 주문상품아이템 컴포넌트 생성 (#84)
    주문하기페이지 주문상품 아이템 컴포넌트를 생성했습니다.
    장바구니 목록을 받아와 출력하는 로직도 추가했습니다.

---
+++ src/containers/orderCheckout/OrderListContainer.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import cartApi from "@/apis/services/cart";
+import OrderItem from "@/components/orderCheckout/OrderItem";
+import { CartItem } from "@/types/cart";
+
+const OrderListContainer = () => {
+  const [cartItems, setCartItems] = useState<CartItem[]>();
+  const fetchAllCartItems = async () => {
+    try {
+      const response = await cartApi.getAllItems();
+      const { item } = response.data;
+      setCartItems(item);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllCartItems();
+  });
+
+  return (
+    <div>
+      {cartItems?.map((item, idx) => {
+        const key = idx.toString();
+        return (
+          <OrderItem
+            key={key}
+            imgUrl={item.product.image}
+            name={item.product.name}
+            quantity={item.quantity}
+            priceSum={item.product.price * item.quantity}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default OrderListContainer;
