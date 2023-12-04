cfe12806431d45fa94a01b0d1ab85c5e262d7c84
jungmyungjin <myung2667@gmail.com>
2023-12-04 17:30:12 +0900

    fix(ItemInput): 컴포넌트 props 속성 수정, 타입 분리 (#38)
    기존 구조 확장을 위해
    input관련 Props inputProps 로 분리,
    buttonValue, buttonDisabled,
    showValidationMessage, validationMessage
    속성을 추가하였습니다.
    
    스타일 수정을 하였습니다.

--- src/components/ItemInput/ItemInput.tsx
+++ src/components/ItemInput/ItemInput.tsx
@@ -1,15 +1,8 @@
 import styled from "styled-components";
+import { PropsWithChildren } from "react";
 import Input from "@/components/common/Input";
 import Button from "@/components/common/Button";
-
-interface CommonCustomStyle {
-  width?: string;
-  height?: string;
-  "font-size"?: string;
-  color?: string;
-  margin?: string;
-  padding?: string;
-}
+import { ItemInputProps } from "@/types/inputItem";
 
 /*
   title : input 타이틀
@@ -25,66 +18,76 @@ interface CommonCustomStyle {
   inputCustomStyle?: input 스타일 세부 설정
   buttonCustomStyle?: button 스타일 세부 설정
  */
-interface ItemInputProps {
-  title?: string | null;
-  isTitleImportant?: boolean;
-  inputValue: string;
-  inputOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  includeButton?: boolean;
-  isInputExpand?: boolean;
-
-  // custom style
-  titleCustomStyle?: CommonCustomStyle;
-  itemInputCustomStyle?: CommonCustomStyle;
-  inputCustomStyle?: CommonCustomStyle;
-  buttonCustomStyle?: CommonCustomStyle;
-}
-
 const ItemInput = ({
   title = null,
   isTitleImportant = false,
-  inputValue = "",
-  inputOnChange = () => {},
+
+  // button
+  buttonValue = "",
+  buttonOnClick = () => {},
+  buttonDisabled = false,
   includeButton = false,
+
+  // input
   isInputExpand = false,
+  inputProps = { value: "", onChange: () => {} },
+  showValidationMessage = false,
+  validationMessage = "",
 
   // custom style
   titleCustomStyle = {},
   itemInputCustomStyle = {},
   inputCustomStyle = {},
   buttonCustomStyle = {},
-}: ItemInputProps) => {
+  children,
+}: PropsWithChildren<ItemInputProps>) => {
   return (
-    <ItemInputLayer $itemInputCustomStyle={itemInputCustomStyle}>
-      {title && (
-        <TitleWrapper $titleCustomStyle={titleCustomStyle}>
-          {title}
-          {isTitleImportant && <IsTitleImportant>*</IsTitleImportant>}
-        </TitleWrapper>
-      )}
-      <InputWrapper $inputCustomStyle={inputCustomStyle}>
-        <Input onChange={inputOnChange} value={inputValue} />
-      </InputWrapper>
-      {includeButton && !isInputExpand && (
-        <ButtonWrapper $buttonCustomStyle={buttonCustomStyle}>
-          {includeButton && <Button value="입력" size="md" variant="sub" />}
-        </ButtonWrapper>
-      )}
+    <ItemInputLayer>
+      <ItemInputWrapper $itemInputCustomStyle={itemInputCustomStyle}>
+        {title && (
+          <TitleWrapper $titleCustomStyle={titleCustomStyle}>
+            {title}
+            {isTitleImportant && <IsTitleImportant>*</IsTitleImportant>}
+          </TitleWrapper>
+        )}
+        {children}
+        <InputWrapper $inputCustomStyle={inputCustomStyle}>
+          <Input {...inputProps} />
+        </InputWrapper>
+        {(includeButton || !isInputExpand) && (
+          <ButtonWrapper onClick={buttonOnClick} $buttonCustomStyle={buttonCustomStyle}>
+            {includeButton && <Button disabled={buttonDisabled} value={buttonValue} size="md" variant="sub" />}
+          </ButtonWrapper>
+        )}
+      </ItemInputWrapper>
+      {showValidationMessage && <ValidationMessage>{validationMessage}</ValidationMessage>}
     </ItemInputLayer>
   );
 };
 
-const ItemInputLayer = styled.div<{ $itemInputCustomStyle: React.CSSProperties }>`
+const ItemInputLayer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+const ItemInputWrapper = styled.div<{ $itemInputCustomStyle: React.CSSProperties }>`
   position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   width: 100%;
   font-size: 1.4rem;
+  margin-bottom: 1rem;
   gap: 1rem;
+
   ${(props) => props.$itemInputCustomStyle && { ...props.$itemInputCustomStyle }}
 `;
 const TitleWrapper = styled.span<{ $titleCustomStyle: React.CSSProperties }>`
+  display: flex;
+
   font-weight: var(--weight-extrabold);
   min-width: 10rem;
   ${(props) => props.$titleCustomStyle && { ...props.$titleCustomStyle }}
@@ -93,15 +96,23 @@ const IsTitleImportant = styled.span`
   color: var(--color-red);
 `;
 const InputWrapper = styled.div<{ $inputCustomStyle: React.CSSProperties }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: auto;
-  height: 100%;
   flex-grow: 1;
   input {
+    padding: 1.2rem;
+    font-size: 1.6rem;
     height: 100%;
-    padding: 1rem;
   }
   ${(props) => props.$inputCustomStyle && { ...props.$inputCustomStyle }}
 `;
+const ValidationMessage = styled.div`
+  color: var(--color-red);
+  font-size: 1.3rem;
+`;
+
 const ButtonWrapper = styled.div<{ $buttonCustomStyle: React.CSSProperties }>`
   width: 11rem;
   font-size: 1.6rem;
