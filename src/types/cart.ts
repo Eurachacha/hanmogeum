26749e19688d2e50eb093e7edbb7d1b4f9e5b63d
youngchae <010101chae@gmail.com>
2023-12-04 16:10:45 +0900

    feat(cart.ts): cart API 요청을 위한 타입 정의 (#85)
    cart API 요청을 위한 타입들을 정의했습니다.

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -8,3 +8,46 @@ export interface CartItemInfo {
   stock: number;
   quantity: number;
 }
+
+export interface CartItemSummary {
+  _id: number;
+  quantity: number;
+}
+
+export interface CartItemDetail {
+  name: string;
+  price: number;
+  image: string;
+}
+
+export interface CartItem {
+  _id: number;
+  product_id: number;
+  quantity: number;
+  createdAt: string;
+  updatedAt: string;
+  product: CartItemDetail;
+}
+
+// Request Types
+
+export interface RequestCartItem {
+  product_id: number;
+  quantity: number;
+}
+
+// Reponse Types
+
+export interface ResponseCartItems {
+  ok: number;
+  item: CartItem[];
+}
+
+export interface ResponseAddCartItem {
+  ok: number;
+  updated: {
+    _id: number;
+    quantity: number;
+    updatedAt: string;
+  };
+}
