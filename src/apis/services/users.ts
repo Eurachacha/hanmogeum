236d12e93d99c28bb3c37bf48fb8b0ba838e627d
jungmyungjin <myung2667@gmail.com>
2023-12-04 17:34:06 +0900

    fix(apis/user): 이메일 중복확인 api추가 (#38)
    회원가입 페이지에서 필요한
    이메일 중복확인 api를 추가하였습니다.

--- src/apis/services/users.ts
+++ src/apis/services/users.ts
@@ -1,7 +1,16 @@
 import { publicInstance, privateInstance } from "../instance";
-import { LoginData, SignUpData, ResponseLogin, ResponseSignUp, ResponseUpdateUser } from "@/types/users";
+import {
+  LoginData,
+  SignUpData,
+  ResponseLogin,
+  ResponseSignUp,
+  ResponseUpdateUser,
+  ResponseEmailDuplicateCheck,
+} from "@/types/users";
 
 const userApi = {
+  emailDuplicateCheck: (email: string) =>
+    publicInstance.get<ResponseEmailDuplicateCheck>(`/users/email?email=${email}`),
   loginUser: (credentials: LoginData) => publicInstance.post<ResponseLogin>("/users/login", credentials),
   signUpUser: (data: SignUpData) => publicInstance.post<ResponseSignUp>("/users", data),
   getUserProfile: (_id: number) => privateInstance.get<ResponseUpdateUser>(`/users/${_id}`),
