cb67b501e3428ef7e29af4b74bd69ec1f608743b
youngchae <010101chae@gmail.com>
2023-12-05 17:50:37 +0900

    feat(orderCheckout): OrderInfo.tsx 컴포넌트 분리 (#84)
    주문하기 페이지 좌측 부분을 OrderInfo.tsx로 분리했습니다.

---
+++ src/containers/orderCheckout/OrderInfo.tsx
@@ -0,0 +1,41 @@
+import { styled } from "styled-components";
+import OrderListContainer from "./OrderListContainer";
+import UserInfoContainer from "@/containers/orderCheckout/UserInfoContainer";
+import ShippingInfoContainer from "./ShippingInfoContainer";
+import { CartItem } from "@/types/cart";
+
+interface OrderInfoProps {
+  cartItems: CartItem[] | undefined;
+}
+
+const OrderInfo = ({ cartItems }: OrderInfoProps) => {
+  return (
+    <>
+      <Section>
+        <SectionTitle>주문 상품</SectionTitle>
+        {cartItems ? <OrderListContainer cartItems={cartItems} /> : <p>상품이 없습니다.</p>}
+      </Section>
+      <Section>
+        <SectionTitle>주문자 정보</SectionTitle>
+        <UserInfoContainer />
+      </Section>
+      <Section>
+        <SectionTitle>배송 정보</SectionTitle>
+        <ShippingInfoContainer />
+      </Section>
+    </>
+  );
+};
+
+export default OrderInfo;
+
+const Section = styled.section`
+  margin: 40px 0;
+`;
+
+const SectionTitle = styled.p`
+  font-size: 1.8rem;
+  font-weight: var(--weight-bold);
+  padding: 12px 4px;
+  border-bottom: 1px solid var(--color-gray-300);
+`;
