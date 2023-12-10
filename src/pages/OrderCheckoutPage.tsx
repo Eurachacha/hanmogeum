33a29f98c9659dc9ef0f0e3b3af07709c521c0e5
youngchae <010101chae@gmail.com>
2023-12-10 20:32:16 +0900

    feat(orderCheckout): [로그인]초기접속시 재고 체크 (#161)
    로그인한 유저가 구매하기페이지 초기 접속 시 재고를 체크하고 부족시 모달을 띄웁니다.
    모달 확인 버튼 클릭 후 이전 페이지(상품상세or장바구니)로 돌아갑니다.

--- src/pages/OrderCheckoutPage.tsx
+++ src/pages/OrderCheckoutPage.tsx
@@ -1,35 +1,56 @@
 import { styled } from "styled-components";
 import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
 import OrderInfo from "@/containers/orderCheckout/OrderInfo";
 import OrderPriceContainer from "@/containers/orderCheckout/OrderPriceContainer";
 import { CartItem } from "@/types/cart";
 import cartApi from "@/apis/services/cart";
 import Button from "@/components/common/Button";
+import { cartCheckedItemState } from "@/recoil/atoms/cartState";
+import loggedInUserState from "@/recoil/atoms/loggedInUserState";
+import Modal from "@/components/common/Modal";
 
 const OrderCheckoutPage = () => {
-  const [cartItems, setCartItems] = useState<CartItem[]>();
+  const user = useRecoilValue(loggedInUserState);
+  const checkedItems = useRecoilValue(cartCheckedItemState);
+  const [cartData, setCartData] = useState<CartItem[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   const fetchAllCartItems = async () => {
     try {
       const response = await cartApi.getAllItems();
-      const { item } = response.data;
-      setCartItems(item);
+      const { item: items } = response.data;
+      setCartData(items.filter((item) => checkedItems.includes(item.product_id)));
+      const isAllInStock = items.every((item) => item.quantity <= item.product.quantity - item.product.buyQuantity);
+      if (!isAllInStock) setIsModalOpen(true);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleModalClose = () => {
+    navigate(-1);
+    setIsModalOpen(false);
+  };
+
   useEffect(() => {
-    fetchAllCartItems();
+    if (user) fetchAllCartItems();
   }, []);
 
   return (
     <OrderCheckoutPageLayer>
+      <Modal isOpen={isModalOpen} message="재고가 부족한 상품이 있습니다.">
+        <ButtonWrapper onClick={handleModalClose}>
+          <Button value="확인" size="sm" variant="sub" />
+        </ButtonWrapper>
+      </Modal>
       <PageLeft>
-        <OrderInfo cartItems={cartItems} />
+        <OrderInfo cartData={cartData} />
       </PageLeft>
       <PageRight>
-        <OrderPriceContainer cartItems={cartItems} />
+        <OrderPriceContainer cartData={cartData} />
         <div>
           <Button value="결제하기" size="lg" variant="point" />
         </div>
@@ -47,6 +68,12 @@ const OrderCheckoutPageLayer = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
   }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: center;
 `;
 
 const PageLeft = styled.div`
