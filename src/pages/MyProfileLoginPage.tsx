7c2633f4a3f6dfe4cbcf37d9b42623445abec308
jungmyungjin <myung2667@gmail.com>
2023-12-10 22:46:18 +0900

    fix(MyProfileLogin): 내 정보 변경 로그인 페이지 구현 (#149)
    내 정보 변경의 로그인 페이지를 구현하였습니다.

---
+++ src/pages/MyProfileLoginPage.tsx
@@ -0,0 +1,12 @@
+import MyProfileLoginContainer from "@/containers/mypageContainer/MyProfileLoginContainer";
+
+const MyProfilePage = () => {
+  return (
+    <div>
+      프로필 페이지(개인정보 조회)
+      <MyProfileLoginContainer />
+    </div>
+  );
+};
+
+export default MyProfilePage;
