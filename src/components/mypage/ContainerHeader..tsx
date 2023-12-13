7b672e36bfffc74edb6530d5b40cd5d41d6a8982
jungmyungjin <myung2667@gmail.com>
2023-12-13 21:07:19 +0900

    fix(MyOrderDetail): 상품 상세페이지 주문 정보 수정(#184)
    상품 상세페이지의 주문 정보 컴포넌트 화면을 수정하였습니다.

--- src/components/mypage/ContainerHeader..tsx
+++ src/components/mypage/ContainerHeader..tsx
@@ -1,19 +1,36 @@
-import styled from "styled-components";
+import styled, { RuleSet, css } from "styled-components";
 
 interface ContainerHeaderProps {
   title: string;
+  variant?: "main" | "sub";
 }
 
-const ContainerHeader = ({ title }: ContainerHeaderProps) => {
-  return <ContainerHeaderLayer>{title}</ContainerHeaderLayer>;
+interface CustomProperties {
+  $variantStyle: RuleSet<object>;
+}
+
+const VARIANTS = {
+  main: css`
+    border-bottom: 1px solid var(--color-black);
+  `,
+  sub: css`
+    border-bottom: 1px solid var(--color-gray-100);
+  `,
 };
 
-const ContainerHeaderLayer = styled.div`
+const ContainerHeader = ({ title, variant = "main" }: ContainerHeaderProps) => {
+  const variantStyle = VARIANTS[variant];
+  return <ContainerHeaderLayer $variantStyle={variantStyle}>{title}</ContainerHeaderLayer>;
+};
+
+const ContainerHeaderLayer = styled.div<CustomProperties>`
+  ${(props) => props.$variantStyle}
   width: 100%;
   height: 5rem;
   font-weight: var(--weight-bold);
   font-size: 2.4rem;
   border-bottom: 1px solid var(--color-black);
+  border-bottom: 1px solid var(--color-gray-200);
 `;
 
 export default ContainerHeader;
