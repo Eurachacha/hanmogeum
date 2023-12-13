381279842c94de936e6d3408d1a27e0c660cf608
Oh5Yeonju <oh5yeonju@gmail.com>
2023-12-13 17:26:13 +0900

    fix(hooks): 디카페인 토글 클릭 시 쿼리스트링 수정(#243)
    디카페인 토글 클릭 시 쿼리스트링에 false 값까지 담기던 오류를 수정했습니다.

--- src/hooks/useQueryParams.tsx
+++ src/hooks/useQueryParams.tsx
@@ -10,13 +10,13 @@ const useQueryParams = (queryKey: string) => {
       setSearchParams(searchParams);
     }
 
-    const isExist = curQueryData[0]?.split(" ").includes(categoryName);
+    const isExist = curQueryData[0]?.split(",").includes(categoryName);
 
     if (isExist) {
       // 해당 카테고리네임이 존재하므로 지워주는 로직
-      const newQueryData = curQueryData[0].split(" ").filter((item) => item !== categoryName);
+      const newQueryData = curQueryData[0].split(",").filter((item) => item !== categoryName);
       searchParams.delete(queryKey);
-      searchParams.append(queryKey, newQueryData.join(" "));
+      searchParams.append(queryKey, newQueryData.join(","));
 
       // 지우고 나서 빈문자열이 되면 queryKey까지 다 지워주는 로직
       if (searchParams.getAll(queryKey)[0] === "") searchParams.delete(queryKey);
@@ -24,9 +24,20 @@ const useQueryParams = (queryKey: string) => {
     } else if (curQueryData[0]) {
       // categoryName이 존재하지는 않지만, 다른 카테고리가 선택되어 있을때
       // 원래 있던 쿼리스트링과 categoryName 사이에 공백을 넣어 다시 세팅
+
       searchParams.delete(queryKey);
-      const newQueryData = `${curQueryData[0]} ${categoryName}`;
+      const newQueryData = `${curQueryData[0]},${categoryName}`;
       searchParams.append(queryKey, newQueryData);
+      setSearchParams(searchParams);
+    }
+  };
+
+  const toggleDecafFilter = (boolean: boolean) => {
+    if (boolean) {
+      searchParams.append(queryKey, boolean.toString());
+      setSearchParams(searchParams);
+    } else {
+      searchParams.delete(queryKey);
       setSearchParams(searchParams);
     }
   };
@@ -35,7 +46,7 @@ const useQueryParams = (queryKey: string) => {
     setSearchParams("");
   };
 
-  return { curQueryData, toggleFilter, resetFilter };
+  return { curQueryData, toggleFilter, toggleDecafFilter, resetFilter };
 };
 
 export default useQueryParams;
