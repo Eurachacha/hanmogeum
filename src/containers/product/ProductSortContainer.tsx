0e9bc431689bcd2218d5b12f67f651e4df0fa850
Oh5Yeonju <oh5yeonju@gmail.com>
2023-12-13 20:06:46 +0900

    feat(ProductList): 상품 목록 카테고리 클릭 시 데이터 요청(#87)
    상품 목록 카테고리 버튼 클릭 시 쿼리스트링을 받아와 데이터를 요청하는
    코드를 추가하였습니다.

--- src/containers/product/ProductSortContainer.tsx
+++ src/containers/product/ProductSortContainer.tsx
@@ -1,24 +1,59 @@
 import { useState, useEffect } from "react";
-import ProductSortButtons from "@/components/product/productlist/ProductSortButtons";
-import ProductItemList from "@/components/product/productlist/ProductItemList";
-import { Product } from "@/types/products";
+import { useLocation, useSearchParams } from "react-router-dom";
 import productsApi from "@/apis/services/products";
+import ProductItemList from "@/components/product/productlist/ProductItemList";
+import ProductSortButtons from "@/components/product/productlist/ProductSortButtons";
+import { Extra, Product } from "@/types/products";
 
 const ProductSortContainer = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
-  const getAllProducts = async () => {
+  const location = useLocation();
+  const queryString = location.search;
+
+  const [searchParams] = useSearchParams(queryString);
+  const packQuery = searchParams.get("pack")?.split(",");
+  const teaTypeQuery = searchParams.get("teaType")?.split(",");
+  const tasteQuery = searchParams.get("taste")?.split(",");
+  const hashTagQuery = searchParams.get("hashTag")?.split(",");
+  const isDecafQuery = searchParams.get("isDecaf")?.split(",");
+
+  type filterObjectType = Partial<Extra>;
+  const filterObject: filterObjectType = {};
+
+  if (packQuery) {
+    filterObject.pack = packQuery;
+  }
+  if (teaTypeQuery) {
+    filterObject.teaType = teaTypeQuery;
+  }
+  if (tasteQuery) {
+    filterObject.taste = tasteQuery;
+  }
+  if (hashTagQuery) {
+    filterObject.hashTag = hashTagQuery;
+  }
+  if (isDecafQuery) {
+    filterObject.isDecaf = Boolean(isDecafQuery[0]);
+  }
+
+  // const sortOrder = { sortBy: "price", sortOrder: -1 };
+  const getFilteredData = async (filterDataObject?: filterObjectType, sortDataObject?: any) => {
     try {
-      const response = await productsApi.getAllProducts();
-      setProducts(response.data.item);
+      const response = await productsApi.searchProducts({
+        sort: sortDataObject,
+        filter: filterDataObject,
+      });
+      const { item } = response.data;
+      setProducts(item);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    getAllProducts();
-  }, []);
+    getFilteredData(filterObject);
+  }, [queryString]);
 
   return (
     <div>
