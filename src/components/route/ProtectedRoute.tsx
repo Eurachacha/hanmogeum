cd0033a9899f4fd8725912d93dcbbe233f20f037
jungmyungjin <myung2667@gmail.com>
2023-12-17 19:41:00 +0900

    feat(route): 라우터 권한 확인하는 layout 컴포넌트 구현(#287)
    Router.tsx에서 사용될 하위 하위 라우터 접근 시 권한을 확인하는 컴포넌트를 구현하였습니다.

---
+++ src/components/route/ProtectedRoute.tsx
@@ -0,0 +1,54 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import useAuthorization from "@/hooks/useAuthorization";
+import Modal from "../common/Modal";
+import Button from "../common/Button";
+import { PrivateRouteProps } from "@/types/routeAuthorization";
+
+const ProtectedRoute = ({ allowedRoles, modalMessage, location }: PrivateRouteProps) => {
+  const navigate = useNavigate();
+  const isAuthenticatedUser = useAuthorization({ allowedRoles });
+  const [modalOpen, setModalOpen] = useState({ isOpen: false, message: "" });
+
+  useEffect(() => {
+    if (modalMessage && modalMessage !== "") {
+      setModalOpen({ isOpen: true, message: modalMessage });
+    } else {
+      navigateLocation();
+    }
+  }, []);
+
+  const navigateLocation = () => {
+    if (typeof location === "number") {
+      navigate(location);
+    } else {
+      navigate(location);
+    }
+  };
+
+  if (!isAuthenticatedUser) {
+    return (
+      <ModalWrapper>
+        <Modal isOpen={modalOpen.isOpen} message={modalOpen.message}>
+          <ButtonWrapper onSubmit={navigateLocation}>
+            <Button value="확인" size="sm" variant="point" />
+          </ButtonWrapper>
+        </Modal>
+      </ModalWrapper>
+    );
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
+
+const ModalWrapper = styled.div`
+  font-weight: var(--weight-semibold);
+  text-align: center;
+`;
+
+const ButtonWrapper = styled.form`
+  width: 100%;
+`;
