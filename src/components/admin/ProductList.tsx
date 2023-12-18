41cb4acabd995f1b17ea2a9794c7b7b069f8972f
youngchae <010101chae@gmail.com>
2023-12-18 09:32:20 +0900

    fix(admin): getCodeValue.ts 내부 훅 제거(#331)
    getCodeValue.ts 내부에서 훅을 사용해 발생하는 경고를 해결하기 위해 훅을 제거했습니다.
    함수를 사용하는 곳에서 훅을 사용해 flattenData를 전달해야 합니다.

--- src/components/admin/ProductList.tsx
+++ src/components/admin/ProductList.tsx
@@ -1,11 +1,15 @@
 import { List, Datagrid, TextField, FunctionField } from "react-admin";
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import { Product } from "@/types/products";
 import getCodeValue from "@/utils/getCodeValue";
 import getPriceFormat from "@/utils/getPriceFormat";
+import { flattenCodeState } from "@/recoil/atoms/codeState";
 
 const ProductList = () => {
+  const flattenCodes = useRecoilValue(flattenCodeState);
   const { authorId } = useParams();
+
   return (
     <List resource="products" filter={{ authorId }}>
       <Datagrid rowClick="edit">
@@ -20,7 +24,7 @@ const ProductList = () => {
         <FunctionField
           label="포장형태"
           render={(record: Product) => {
-            return getCodeValue(record.extra.pack[0]);
+            return getCodeValue(flattenCodes, record.extra.pack[0]);
           }}
         />
         <TextField source="buyQuantity" label="누적주문수" sortable />
