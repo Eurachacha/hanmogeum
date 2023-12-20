bd199dd1032b0f5091a717609697239022b48ff2
youngchae <010101chae@gmail.com>
2023-12-21 03:16:40 +0900

    feat(seller): 상품리스트 폰트사이즈 조절(#444)
    상품관리 상품목록 페이지 폰트사이즈를 조절했습니다.

--- src/components/admin/ProductList.tsx
+++ src/components/admin/ProductList.tsx
@@ -11,23 +11,49 @@ const ProductList = () => {
   const { authorId } = useParams();
 
   return (
-    <List resource="products" filter={{ authorId }}>
+    <List
+      resource="products"
+      filter={{ authorId }}
+      sx={{
+        "& th, td ": {
+          fontSize: "1.4rem",
+          textAlign: "center",
+        },
+        "& th": {
+          padding: "12px 0",
+          fontWeight: 600,
+        },
+        "td > span": {
+          fontSize: "1.2rem",
+        },
+        "& td.column-price > span": {
+          textAlign: "end",
+        },
+      }}
+    >
       <Datagrid rowClick="edit">
         <TextField source="_id" label="상품ID" sortable={false} />
         <TextField source="name" label="상품명" sortable={false} />
         <FunctionField
+          source="price"
           label="판매가격"
           render={(record: Product) => getPriceFormat({ price: record.price })}
           sortable
         />
-        <FunctionField label="재고" render={(record: Product) => record.quantity - record.buyQuantity} sortable />
         <FunctionField
+          source="stock"
+          label="재고"
+          render={(record: Product) => record.quantity - record.buyQuantity}
+          sortable
+        />
+        <FunctionField
+          source="extra.pack"
           label="포장형태"
           render={(record: Product) => {
             return getCodeValue(flattenCodes, record.extra.pack[0]);
           }}
         />
-        <TextField source="buyQuantity" label="누적주문수" sortable />
+        <TextField source="buyQuantity" label="누적주문수" sortable={false} />
       </Datagrid>
     </List>
   );
