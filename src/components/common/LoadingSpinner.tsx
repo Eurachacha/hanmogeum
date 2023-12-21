df8c6e5de73152eca18d799bf96c1e4acd4ea061
jungmyungjin <myung2667@gmail.com>
2023-12-22 06:15:44 +0900

    feat(LoadingSpinner): 로딩 스피너 컴포넌트 추가(#475)
    suspense 상태 시 돌아갈 로딩스피너 컴포넌트를 추가하였습니다.

---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,72 @@
+import styled, { keyframes } from "styled-components";
+
+// Keyframes for the animation
+const animloader = keyframes`
+  0% {
+    box-shadow: 2px 0px rgba(210, 89, 37, 0), 12px 0px rgba(210, 89, 37, 0.3), 20px 0px rgba(210, 89, 37, 0);
+  }
+  50% {
+    box-shadow: 2px -5px rgba(210, 89, 37, 0.5), 12px -3px rgba(210, 89, 37, 0.5), 20px -2px rgba(210, 89, 37, 0.6);
+  }
+  100% {
+    box-shadow: 2px -8px rgba(210, 89, 37, 0), 12px -5px rgba(210, 89, 37, 0), 20px -5px rgba(210, 89, 37, 0);
+  }
+`;
+
+// Styled loader component
+const Loader = styled.div`
+  width: 48px;
+  height: 40px;
+  margin-top: 30px;
+  display: inline-block;
+  position: relative;
+  background: #d25925;
+  border-radius: 15% 15% 35% 35%;
+
+  &::after {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    left: 45px;
+    top: 8px;
+    border: 4px solid #d25925;
+    width: 16px;
+    height: 20px;
+    border-radius: 0 4px 4px 0;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 1px;
+    height: 10px;
+    top: -15px;
+    left: 11px;
+    box-sizing: border-box;
+    animation: ${animloader} 1s ease infinite;
+  }
+`;
+
+const SpinnerWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  z-index: 2000;
+  & > div {
+    margin-top: 20rem;
+  }
+`;
+
+const LoadingSpinner = () => {
+  return (
+    <SpinnerWrapper>
+      <Loader />
+    </SpinnerWrapper>
+  );
+};
+
+export default LoadingSpinner;
