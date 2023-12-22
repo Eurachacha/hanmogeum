83397464ec622150745b0444c4cff15d27e5abcd
Oh5Yeonju <oh5yeonju@gmail.com>
2023-12-22 12:28:32 +0900

    fix(MainPage): 메인페이지 배너 디자인 수정(#471)
    메인 페이지 띠배너 디자인을 수정하였습니다.

--- src/components/main/Banner.tsx
+++ src/components/main/Banner.tsx
@@ -10,11 +10,12 @@ const Banner = () => {
   };
 
   return (
-    <BannerLayer>
-      <StyledBannerTitle>내 취향의 차를 찾아보세요.</StyledBannerTitle>
-      <StyledBannerButton onClick={moveToRecommendPage}>
-        <Button value="추천 받으러 가기" size="sm" variant="point"></Button>
-      </StyledBannerButton>
+    <BannerLayer onClick={moveToRecommendPage}>
+      <BannerTextWrapper>
+        <h2>내 취향에 차를 알고 싶다면?</h2>
+        <p>한모금에서 입맛에 맞는 차를 찾아드려요.</p>
+        <Button value="차 추천 받기" size="sm" variant="point" />
+      </BannerTextWrapper>
     </BannerLayer>
   );
 };
@@ -22,19 +23,23 @@ const Banner = () => {
 export default Banner;
 
 const BannerLayer = styled.div`
-  padding: 80px 20px;
-  background-color: #ffdccc;
+  background-color: #f9f7e8;
+  height: 200px;
 `;
 
-const StyledBannerTitle = styled.h2`
-  font-size: 2.4rem;
-  font-weight: var(--weight-regular);
-  margin-bottom: 20px;
-`;
-// TODO: button으로 변경 시 background 이슈
-const StyledBannerButton = styled.div`
-  width: 200px;
-  background-color: none;
-  border: none;
-  box-sizing: border-box;
+const BannerTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    margin-top: 40px;
+    font-size: 2.4rem;
+  }
+  p {
+    margin: 12px 0 24px 0;
+  }
+  button {
+    width: 200px;
+  }
 `;
