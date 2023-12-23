a6d0461a56779ef4aa20e974b5ecd51268dd93d5
jungmyungjin <myung2667@gmail.com>
2023-12-23 22:13:51 +0900

    bug(useAuthorization): 경로 이동시 권한체크 되도록 수정(#500)
    뒤로가기 등 경로 이동 시 권한체크하도록 수정하였습니다.

--- src/hooks/useAuthorization.ts
+++ src/hooks/useAuthorization.ts
@@ -1,5 +1,6 @@
 import { useRecoilValue } from "recoil";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { getUserTypeState } from "@/recoil/selectors/loggedInUserSelector";
 import { AuthorizationProps } from "@/types/routeAuthorization";
 import additionalAuthState from "@/recoil/atoms/additionalAuthState";
@@ -11,6 +12,7 @@ import additionalAuthState from "@/recoil/atoms/additionalAuthState";
  */
 const useAuthorization = ({ allowedRoles, isAdditionalAuthRequired = false }: AuthorizationProps) => {
   const userType = useRecoilValue(getUserTypeState);
+  const location = useLocation();
   const additionalAuthRequired = useRecoilValue(additionalAuthState);
   const [resultValue, setResultValue] = useState(false);
 
@@ -24,12 +26,13 @@ const useAuthorization = ({ allowedRoles, isAdditionalAuthRequired = false }: Au
     } else if (allowedRoles.includes("guest") && userType === null) {
       setResultValue(true);
     }
-
     // 입력한 권한과 사용자 권한이 일치하는 경우
     else if (userType && allowedRoles.includes(userType)) {
       setResultValue(true);
+    } else {
+      setResultValue(false);
     }
-  }, [userType, additionalAuthRequired]);
+  }, [location, userType, additionalAuthRequired]);
 
   return resultValue;
 };
