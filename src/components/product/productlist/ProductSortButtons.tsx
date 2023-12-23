2235d9ab561b8026d6d2fbd5b8d7cfb7a92f0da0
Oh5Yeonju <oh5yeonju@gmail.com>
2023-12-24 00:03:44 +0900

    fix(ProductList): 선택한 정렬버튼 CSS 추가(#517)
    선택한 정렬 버튼에 CSS를 추가해 선택하지 않은 버튼과 차이를 뒀습니다.

--- src/components/product/productlist/ProductSortButtons.tsx
+++ src/components/product/productlist/ProductSortButtons.tsx
@@ -1,17 +1,33 @@
-import styled from "styled-components";
+import styled, { RuleSet, css } from "styled-components";
+import { useLocation } from "react-router-dom";
 import useQueryParams from "@/hooks/useQueryParams";
 
 interface ProductSortButtonsProps {
   productLength: number;
 }
 const ProductSortButtons = ({ productLength }: ProductSortButtonsProps) => {
+  const VARIENTS = {
+    active: css`
+      color: var(--color-gray-500);
+      font-weight: var(--weight-bold);
+    `,
+    default: css`
+      color: var(--color-gray-300);
+    `,
+  };
   const categoryArray = [
     { name: "인기순", sortNumber: 0 },
     { name: "최신순", sortNumber: 1 },
     { name: "가격낮은순", sortNumber: 2 },
     { name: "가격높은순", sortNumber: 3 },
   ];
+
   const { toggleSortFilter } = useQueryParams("sortType");
+
+  const location = useLocation();
+  const queryString = location.search;
+  const searchParams = new URLSearchParams(queryString);
+  const sortTypeKey = searchParams.get("sortType");
 
   return (
     <CategorySortLayer>
@@ -20,7 +36,11 @@ const ProductSortButtons = ({ productLength }: ProductSortButtonsProps) => {
       </li>
       <li>
         {categoryArray.map((cate) => (
-          <StyledCategorySortButton key={cate.name} onClick={() => toggleSortFilter(`${cate.sortNumber}`)}>
+          <StyledCategorySortButton
+            key={cate.name}
+            onClick={() => toggleSortFilter(`${cate.sortNumber}`)}
+            $variantStyle={sortTypeKey && +sortTypeKey === cate.sortNumber ? VARIENTS.active : VARIENTS.default}
+          >
             {cate.name}
           </StyledCategorySortButton>
         ))}
@@ -42,13 +62,13 @@ const StyledProductCount = styled.p`
   color: var(--color-gray-300);
 `;
 
-const StyledCategorySortButton = styled.button`
+const StyledCategorySortButton = styled.button<{ $variantStyle: RuleSet<object> }>`
+  ${(props) => props.$variantStyle}
   background: none;
   padding: 4px 8px;
   border: none;
 
   font-size: 1.2rem;
-  color: var(--color-gray-300);
 
   cursor: pointer;
 `;
